0d8c02aac66a89bc5acd75c8190cf641cdb379d7
rohit-misra-dol-73 <rohit.misra.1@protonmail.com>
2021-02-04 13:11:24 +0530

    Test Java File Uploaded

--- java/test1.java
+++ java/test1.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*
 void Main()
 {
-	 public static void main(String args[]){  
-     System.out.println("Hello Java!");  
-    }  
+	 public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			System.out.println("Hello Java!");
+		}
 }
